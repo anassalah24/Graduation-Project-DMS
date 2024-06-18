8c31bedead37c75851d340bc5ea294fac3b8b03e
Anas Salah <68183749+anassalah24@users.noreply.github.com>
2024-06-18 20:20:24 +0300

    finalized AI model benchmarking metrics , contributing to #5

--- Jetson SW/include/inferseq.h
+++ Jetson SW/include/inferseq.h
@@ -7,6 +7,21 @@
 #include <mutex>
 #include <NvInfer.h>
 #include <opencv2/opencv.hpp>
+#include <cuda_runtime_api.h> // For cudaMemGetInfo
+#include <cuda_runtime.h>
+#include <unistd.h>           // For sysconf and _SC_PAGESIZE
+
+#include <fstream>
+#include <sstream>
+#include <unistd.h> // For sleep function
+
+
+
+struct CPUUsage {
+    long idleTime;
+    long totalTime;
+};
+
 
 using namespace nvinfer1;
 
@@ -17,6 +32,7 @@ class Logger : public ILogger {
 } gLogger;
 
 class TRTEngineSingleton {
+
 private:
     static TRTEngineSingleton* instance;
     static std::mutex mtx; // Mutex for thread safety
@@ -27,7 +43,24 @@ private:
         loadEngines(); // Initial engine loading
     }
 
+    size_t peakHeadPoseGpuMemoryUsage = 0;
+    size_t peakEyeGazeGpuMemoryUsage = 0;
+
+	size_t totalCpuMemoryUsageHeadPose = 0;
+	size_t totalCpuMemoryUsageEyeGaze = 0;
+	size_t headPoseInferenceCount = 0;
+	size_t eyeGazeInferenceCount = 0;
+	size_t totalCpuUsageEyeGaze = 0;
+	size_t totalCpuUsageHeadPose = 0;
+
+
+
+
+
+
 public:
+
+
     static TRTEngineSingleton* getInstance() {
         std::lock_guard<std::mutex> lock(mtx);
         if (!instance) {
@@ -100,6 +133,14 @@ public:
         return std::vector<float>(9, -100);
     }
 
+
+    size_t memUsageBefore = getHostMemoryUsage();
+    CPUUsage cpuUsageBefore = getCPUUsage();
+
+
+
+
+
     // Preprocessing
     cv::Mat resizedImage;
     cv::resize(croppedFace, resizedImage, cv::Size(224, 224));  // Assume input size is 224x224
@@ -123,6 +164,9 @@ public:
     cudaMalloc(&gpuInputBuffer, batchSize * inputSize * sizeof(float));
     cudaMalloc(&gpuOutputBuffer, batchSize * outputSize * sizeof(float));
 
+    size_t freeMemBefore, totalMemBefore, freeMemAfter, totalMemAfter;
+    cudaMemGetInfo(&freeMemBefore, &totalMemBefore); // Get free memory before inference
+
     // Copy data to GPU
     cudaMemcpy(gpuInputBuffer, resizedImage.ptr<float>(), inputSize * sizeof(float), cudaMemcpyHostToDevice);
 
@@ -134,6 +178,36 @@ public:
     // Retrieve results
     std::vector<float> results(outputSize);
     cudaMemcpy(results.data(), gpuOutputBuffer, outputSize * sizeof(float), cudaMemcpyDeviceToHost);
+
+	cudaMemGetInfo(&freeMemAfter, &totalMemAfter); // Get free memory after inference
+
+	size_t usedMemDuringInference = (freeMemBefore - freeMemAfter);
+	if (usedMemDuringInference > peakHeadPoseGpuMemoryUsage) {
+		peakHeadPoseGpuMemoryUsage = usedMemDuringInference;
+	}
+
+    size_t memUsageAfter = getHostMemoryUsage();
+
+
+    size_t memUsageDelta = memUsageAfter - memUsageBefore;
+
+
+    totalCpuMemoryUsageHeadPose += memUsageDelta;
+    headPoseInferenceCount++;
+ 
+	CPUUsage cpuUsageAfter = getCPUUsage();
+
+
+
+    double cpuUsage = calculateCPUUsage(cpuUsageBefore, cpuUsageAfter);
+    totalCpuUsageHeadPose += cpuUsage;
+
+
+
+
+
+
+
 
     // Cleanup
     cudaFree(gpuInputBuffer);
@@ -149,6 +223,12 @@ public:
         std::cerr << "Eye gaze engine not loaded, using default values." << std::endl;
         return std::vector<float>(9, -100);
     }
+
+    size_t memUsageBefore = getHostMemoryUsage();
+
+    CPUUsage cpuUsageBefore = getCPUUsage();
+
+
 
     // Preprocessing
     cv::Mat resizedImage;
@@ -173,6 +253,10 @@ public:
     cudaMalloc(&gpuInputBuffer, batchSize * inputSize * sizeof(float));
     cudaMalloc(&gpuOutputBuffer, batchSize * outputSize * sizeof(float));
 
+    size_t freeMemBefore, totalMemBefore, freeMemAfter, totalMemAfter;
+    cudaMemGetInfo(&freeMemBefore, &totalMemBefore); // Get free memory before inference
+
+
     // Copy data to GPU
     cudaMemcpy(gpuInputBuffer, resizedImage.ptr<float>(), inputSize * sizeof(float), cudaMemcpyHostToDevice);
 
@@ -185,6 +269,43 @@ public:
     std::vector<float> results(outputSize);
     cudaMemcpy(results.data(), gpuOutputBuffer, outputSize * sizeof(float), cudaMemcpyDeviceToHost);
 
+
+
+    cudaMemGetInfo(&freeMemAfter, &totalMemAfter); // Get free memory after inference
+
+    size_t usedMemDuringInference = (freeMemBefore - freeMemAfter);
+    if (usedMemDuringInference > peakEyeGazeGpuMemoryUsage) {
+        peakEyeGazeGpuMemoryUsage = usedMemDuringInference;
+    }
+
+    size_t memUsageAfter = getHostMemoryUsage();
+
+
+    size_t memUsageDelta = memUsageAfter - memUsageBefore;
+
+
+    totalCpuMemoryUsageEyeGaze += memUsageDelta;
+    eyeGazeInferenceCount++;
+
+
+	CPUUsage cpuUsageAfter = getCPUUsage();
+
+
+
+
+    double cpuUsage = calculateCPUUsage(cpuUsageBefore, cpuUsageAfter);
+
+
+
+
+    totalCpuUsageEyeGaze += cpuUsage;
+
+
+
+
+
+
+
     // Cleanup
     cudaFree(gpuInputBuffer);
     cudaFree(gpuOutputBuffer);
@@ -192,6 +313,91 @@ public:
 
     return results;
 }
+
+    size_t getPeakHeadPoseGpuMemoryUsage() const {
+        return peakHeadPoseGpuMemoryUsage;
+    }
+
+    size_t getPeakEyeGazeGpuMemoryUsage() const {
+        return peakEyeGazeGpuMemoryUsage;
+    }
+
+    size_t gettotalCpuMemoryUsageHeadPose() const {
+        return totalCpuMemoryUsageHeadPose;
+    }
+
+    size_t gettotalCpuMemoryUsageEyeGaze() const {
+        return totalCpuMemoryUsageEyeGaze;
+    }
+    size_t getheadPoseInferenceCount() const {
+        return headPoseInferenceCount;
+    }
+
+    size_t geteyeGazeInferenceCount() const {
+        return eyeGazeInferenceCount;
+    }
+
+    size_t geteyeGazeCpuUsage() const {
+        return totalCpuUsageEyeGaze;
+    }
+
+    size_t getheadPoseCpuUsage() const {
+        return totalCpuUsageHeadPose;
+    }
+
+
+    void resetPeakGpuMemoryUsage() {
+        peakHeadPoseGpuMemoryUsage = 0;
+        peakEyeGazeGpuMemoryUsage = 0;
+		totalCpuMemoryUsageHeadPose = 0;
+        totalCpuMemoryUsageEyeGaze = 0;
+		headPoseInferenceCount = 0;
+		eyeGazeInferenceCount = 0;
+        totalCpuUsageEyeGaze = 0;		
+        totalCpuUsageHeadPose = 0;	
+
+    }
+
+	size_t getHostMemoryUsage() {
+		std::ifstream statm("/proc/self/statm");
+		size_t size, resident, share, text, lib, data, dt;
+		statm >> size >> resident >> share >> text >> lib >> data >> dt;
+		statm.close();
+		return resident * sysconf(_SC_PAGESIZE); // Return resident set size in bytes
+	}
+
+	size_t getPageFaults() {
+		std::ifstream stat("/proc/self/stat");
+		std::string dummy;
+		size_t page_faults;
+		for (int i = 0; i < 10; ++i) stat >> dummy; // Skip first 9 fields
+		stat >> page_faults; // Get the 10th field (minor page faults)
+		stat.close();
+		return page_faults;
+	}
+
+	CPUUsage getCPUUsage() {
+		std::ifstream file("/proc/stat");
+		std::string line;
+		std::getline(file, line);
+		std::istringstream ss(line);
+		std::string cpu;
+		long user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
+		ss >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal >> guest >> guest_nice;
+		file.close();
+
+		long idleTime = idle + iowait;
+		long totalTime = user + nice + system + idle + iowait + irq + softirq + steal;
+
+		return {idleTime, totalTime};
+	}
+
+	double calculateCPUUsage(const CPUUsage& prev, const CPUUsage& curr) {
+		long totalDiff = curr.totalTime - prev.totalTime;
+		long idleDiff = curr.idleTime - prev.idleTime;
+		return 100.0 * (totalDiff - idleDiff) / totalDiff;
+	}
+
 
 
 
@@ -207,6 +413,7 @@ public:
             engine2 = nullptr;
         }
     }
+
 
 private:
     ICudaEngine* loadEngine(const std::string& engineFile) {
